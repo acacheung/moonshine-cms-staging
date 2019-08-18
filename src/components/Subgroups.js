8ef7a4331f46e8cf2ec9cda1c80decd06475ff6b
Amanda Cheung <acacheung@gmail.com>
2019-08-18 00:20:33 -0700

    Add menu items

--- src/components/Subgroups.js
+++ src/components/Subgroups.js
@@ -8,13 +8,15 @@ const Subgroups = ({ subgroups }) => (
       <article key={v4()}>
         <h2 className="menu__heading">{subgroup.title}</h2>
         <ul className="menu__list">
-          <li className="menu__item">
-            <h3 className="menu__item__heading">
-              Oysters on the Half Shell
-              <span className="menu__item__price">3 each</span>
-            </h3>
-            <p className="menu__item__desc">Classic Cocktail Sauce, Chili Cucumber Mignonette</p>
-          </li>
+          {subgroup.items.map(item => (
+            <li className="menu__item" key={v4()}>
+              <h3 className="menu__item__heading">
+                {item.title}
+                <span className="menu__item__price">{item.price}</span>
+              </h3>
+              <p className="menu__item__desc">{item.description}</p>
+            </li>
+          ))}
         </ul>
       </article>
     ))}
